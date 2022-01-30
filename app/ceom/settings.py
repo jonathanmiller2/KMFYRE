d34836b1a67423031014d6f4491ce8930e2ac4a4
Jonathan Miller <jonathanmiller2@hotmail.com>
2022-01-29 20:59:26 -0600

    Django settings modification

--- app/ceom/settings.py
+++ app/ceom/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(__file__)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!%5ff!pm7n+pr$*_q1j7h&h@q!$#d+1t_5_e5&q9sd!j6=i4ro'
+SECRET_KEY = os.environ.get("DJANGO_SECURE_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DJANGO_DEBUG", default=0))
