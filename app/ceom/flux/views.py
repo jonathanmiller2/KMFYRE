637563cd86670174f08df0593c3679608667685d
Mariah Vadakekut <mariah.vadak@ou.edu>
2022-03-31 19:56:09 -0500

    Recent changes

--- app/ceom/flux/views.py
+++ app/ceom/flux/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, template_name = 'flux/base.html')
+    return render(request, template_name = 'flux/index.html')
 
     
